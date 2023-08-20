05bf066738cdc3604f734a82257af0ea2f0b0524
dusklight00 <r.rahul.developer@gmail.com>
2023-08-20 16:21:25 +0530

    getting-started-page converted to react #19

---
+++ src/pages/GettingStartedPage/GettingStartedPage.tsx
@@ -0,0 +1,25 @@
+import "./GettingStartedPage.css"
+import { IonImg, IonPage } from "@ionic/react";
+
+const GettingStartedPage: React.FC = () => {
+  return (
+    <IonPage className="ion-page">
+      <div className="container">
+          <div className="logo">
+              <img src="src/pages/img/logo.png" alt="logo" />
+          </div>
+          <div className="welcome-background">
+              <img src="src/pages/img/welcome.png" alt="background-image" />
+          </div>
+          <div className="quote">
+              Be the part of the solution not the part <br />
+              of the pollution
+          </div>
+          <div className="cta-getting-started">Getting Started</div>
+          <div className="sub-cta-line">E-Waste | Large Appliance</div>
+      </div>
+    </IonPage>
+  );
+};
+
+export default GettingStartedPage;
