14f4f1b9673db3cec0186c6b69f58a1e1214c113
dusklight00 <r.rahul.developer@gmail.com>
2023-09-03 12:03:49 +0530

    getting started page images fixed #23

--- src/pages/GettingStartedPage/GettingStartedPage.tsx
+++ src/pages/GettingStartedPage/GettingStartedPage.tsx
@@ -1,15 +1,18 @@
 import "./GettingStartedPage.css"
 import { IonImg, IonPage } from "@ionic/react";
 
+import Logo from "./img/logo.png";
+import Welcome from "./img/welcome.png";
+
 const GettingStartedPage: React.FC = () => {
   return (
     <IonPage className="ion-page">
       <div className="container">
           <div className="logo">
-              <img src="src/pages/GettingStartedPage/img/logo.png" alt="logo" />
+              <img src={Logo} alt="logo" />
           </div>
           <div className="welcome-background">
-              <img src="src/pages/GettingStartedPage/img/welcome.png" alt="background-image" />
+              <img src={Welcome} alt="background-image" />
           </div>
           <div className="quote">
               Be the part of the solution not the part <br />
