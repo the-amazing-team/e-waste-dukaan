0da5eab795d3023d04948174972bb588d49f3610
dusklight00 <r.rahul.developer@gmail.com>
2023-09-03 14:28:02 +0530

    fixed images in logo selector page

--- src/pages/LoginSelectorPage/LoginSelectorPage.tsx
+++ src/pages/LoginSelectorPage/LoginSelectorPage.tsx
@@ -1,12 +1,14 @@
 import "./LoginSelectorPage.css"
 import { IonPage } from "@ionic/react";
 
+import Logo from "./img/logo.png"
+
 const LoginSelectorPage: React.FC = () => {
   return (
     <IonPage className="ion-page">
       <div className="container">
         <div className="logo">
-            <img src="src/pages/LoginSelectorPage/img/logo.png" alt="logo" />
+            <img src={Logo} alt="logo" />
         </div>
         <div className="sub-heading">
             Be the contributor to<br />
